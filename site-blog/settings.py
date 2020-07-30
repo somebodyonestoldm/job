2ad31ec74a628c2842841da3673c61f761887f28
somebodyonestoldm <elder.eret@gmail.com>
2020-07-30 23:08:22 +0300

    'merge'

--- site-blog/settings.py
+++ site-blog/settings.py
@@ -125,4 +125,4 @@ MEDIA_URL = '/media/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
-MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), "media")
+MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR),"site-blog/media")
